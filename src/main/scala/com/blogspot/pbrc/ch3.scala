801155c07b80504a9a5cc1b3e1ef1f5727f7597b
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-22 13:19:47 +0100

    Chapter 3 exercises 1-6

---
+++ src/main/scala/com/blogspot/pbrc/ch3.scala
@@ -0,0 +1,65 @@
+package com.blogspot.pbrc
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](xs: List[A]): List[A] = xs match {
+    case Cons(h, t) => t
+    case _ => Nil
+  }
+
+  def setHead[A](xs: List[A], head: A): List[A] = xs match {
+    case Cons(oldHead, t) => Cons(head, t)
+    case _ => Cons(head, Nil)
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Cons(h, t) => {
+      if (n > 0) drop(t, n - 1)
+      else l
+    }
+    case _ => Nil
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l  
+  }
+
+  def init[A](l: List[A]):List[A] = l match {
+    case Cons(h, Nil) => List()
+    case Cons(h, Cons(t, Nil)) => Cons(h, Nil)
+    case Cons(h, t) => Cons(h, init(t))  
+  }
+
+}
+
+object Chapter3 {
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + List.sum(t)
+    case _ => 101
+  }
+
+}
---
+++ src/test/scala/com/blogspot/pbrc/Chapter3Suite.scala
@@ -0,0 +1,36 @@
+package com.blogspot.pbrc
+
+import com.blogspot.pbrc.Chapter1._
+import org.scalatest.FunSuite
+
+class Chapter3Suite extends FunSuite {
+
+  test("tail removes the head of a list") {
+    assert(List(2, 3) == List.tail(List(1, 2, 3)))
+  }
+
+  test("tail on empty List is Nil") {
+    assert(Nil == List.tail(Nil))
+  }
+
+  test("setHead replaces the head of a list") {
+    assert(List(4, 2, 3) == List.setHead(List(1, 2, 3), 4))
+  }
+
+  test("drop 2 removes the first to elements of a list") {
+    assert(List(3) == List.drop(List(1, 2, 3), 2))
+  }
+
+  test("drop 2 on a one element list gives an empty list") {
+    assert(List() == List.drop(List(1), 2))
+  }
+
+  test("dropWhile removes elements of a list while predicate is true") {
+    assert(List(3) == List.dropWhile(List(1,2,3), (e:Int) => e < 3))
+  }
+
+  test("init removes the last element from a list") {
+    assert(List(1,2) == List.init(List(1,2,3)))
+  }
+
+}
