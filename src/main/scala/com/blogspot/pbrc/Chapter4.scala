9354ea50f3314fd6effa9945d447355e6c233d03
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-23 15:24:26 +0100

    Chapter 4: Exercise 2

--- src/main/scala/com/blogspot/pbrc/Chapter4.scala
+++ src/main/scala/com/blogspot/pbrc/Chapter4.scala
@@ -20,3 +20,15 @@ sealed trait Option[+A] {
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Chapter4 {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
+}
