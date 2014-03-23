ba842d20ed23eba9a0f1ddddc00e62f8479096d4
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-23 18:04:48 +0100

    Chapter 4: Excercise 6

--- src/main/scala/com/blogspot/pbrc/Chapter4.scala
+++ src/main/scala/com/blogspot/pbrc/Chapter4.scala
@@ -35,6 +35,27 @@ object Option {
   def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
 }
 
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  //from: https://github.com/pchiusano/fpinscala/blob/master/answerkey/errorhandling/6.answer.scala
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for { a <- this; b1 <- b } yield f(a, b1)
+}
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
 object Chapter4 {
 
   def mean(xs: Seq[Double]): Option[Double] =
