df9ba4e1311962562e3aab5f3c7ef6b9bc503bb5
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-23 15:10:34 +0100

    Chapter 4: Exercise 1 in terms of flatMap and getOrElse

--- src/main/scala/com/blogspot/pbrc/Chapter4.scala
+++ src/main/scala/com/blogspot/pbrc/Chapter4.scala
@@ -6,26 +6,16 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
     case None => None
   }
-
-  def flatMap[B](f: A => Option[B]): Option[B] = this match {
-    case None => None
-    case Some(a) => f(a)
-  }
-
   def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-    case None => ob
-    case Some(_) => this
-  }
-  def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) if f(a) => this
-    case _ => None
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  }
+  def orElse[B >: A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 }
 
 case class Some[+A](get: A) extends Option[A]
