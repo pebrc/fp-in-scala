d3c5388a7b1929e3d40df8e0e9a79a264be3d22f
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-23 16:55:49 +0100

    Chapter 4: Exercise 5

--- src/main/scala/com/blogspot/pbrc/Chapter4.scala
+++ src/main/scala/com/blogspot/pbrc/Chapter4.scala
@@ -30,6 +30,9 @@ object Option {
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a.foldRight(Some(Nil): Option[List[A]])(map2(_, _)((a, b) => a :: b))
 
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a.foldRight(Some(Nil): Option[List[B]])((x, y) => map2(f(x), y)(_ :: _))
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
 }
 
 object Chapter4 {
