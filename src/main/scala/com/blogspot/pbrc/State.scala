6c46fbf68380640d6d65fd588f343617ba658ae3
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-04-29 19:54:48 +0200

    Chapter 6 Exercise 9

---
+++ src/main/scala/com/blogspot/pbrc/State.scala
@@ -0,0 +1,32 @@
+package fpinscala.functionalstate
+
+case class State[S, +A](run: S => (A, S)) {
+
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => State.unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s0 => {
+      val (a, s1) = run(s0)
+      f(a).run(s1)
+    })
+
+}
+
+object State {
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+
+    fs.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+}
