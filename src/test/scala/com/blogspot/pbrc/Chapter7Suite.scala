d7b6488e5889a949378383a14d134e98c6a6900e
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-05-18 09:56:45 +0200

    Addded test for map2Future

--- src/test/scala/com/blogspot/pbrc/Chapter7Suite.scala
+++ src/test/scala/com/blogspot/pbrc/Chapter7Suite.scala
@@ -9,6 +9,22 @@ class Chapter7Suite extends FunSuite with BeforeAndAfterAll {
 
   private val es = Executors.newCachedThreadPool()
 
+  test("map2WithTimeouts respects time outs") {
+    def tooLong = {
+      TimeUnit.MILLISECONDS.sleep(60)
+      ()
+    }
+    def justRight = {
+      TimeUnit.MILLISECONDS.sleep(30)
+      ()
+    }
+    val par = map2WithTimeouts(fork(unit(tooLong)), fork(unit(justRight)))((a, b) => { "Done" })(es)
+
+    intercept[TimeoutException] {
+      par.get(50, TimeUnit.MILLISECONDS)
+    }
+  }
+
   test("parFilter filters list elements") {
     parFilter((1 to 10).toList)(_ % 2 == 0)(es).get should be(List(2, 4, 6, 8, 10))
   }
