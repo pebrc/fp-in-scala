d23bd5e283255522d4c2775cd7d257ad591ccaf4
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-06-01 21:20:11 +0200

    Chapter 8: Ex. 6 sameParity

--- src/main/scala/fpinscala/testing/testing.scala
+++ src/main/scala/fpinscala/testing/testing.scala
@@ -26,8 +26,17 @@ trait Prop {
 }
 
 case class Gen[A](sample: State[RNG, A]) {
+
+  def map[B](f: A => B): Gen[B] = {
+    Gen(sample.map(f))
+  }
+
   def flatMap[B](f: A => Gen[B]): Gen[B] = {
     Gen(sample.flatMap(f.andThen(_.sample)))
+  }
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = {
+    size.flatMap(n => Gen.listOfN(n, this))
   }
 }
 
@@ -44,8 +53,17 @@ object Gen {
     Gen(State(RNG.nonNegativeInt).map(_ % 2 == 0))
   }
 
+  def sameParity(from: Int, to: Int): Gen[(Int, Int)] = {
+    for {
+      first <- choose(from, to)
+      second <- choose(from, to - 1) //make sure we are still in range when we have to change parity
+      // If x and y are two integers for which x+y is even, then x and y have the same parity
+    } yield (first, if ((first + second) % 2 == 0) second else second + 1)
+  }
+
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
     //this is the reference solution
     Gen(State.sequence(List.fill(n)(g.sample)))
   }
+
 }
