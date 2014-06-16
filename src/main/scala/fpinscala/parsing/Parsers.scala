a21c76a57d5fee7ea8eece742c877ec08dfc8dfa
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-06-16 20:58:00 +0200

    Chapter 9 map in terms of flatMap

--- src/main/scala/fpinscala/parsing/Parsers.scala
+++ src/main/scala/fpinscala/parsing/Parsers.scala
@@ -30,7 +30,8 @@ trait Parsers[ParserError, Parser[+_]] {
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _) or succeed(List())
 
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] =
+    flatMap(a)(v => succeed(f(v)))
 
   def succeed[A](a: A): Parser[A] = string("").map(_ => a)
 
