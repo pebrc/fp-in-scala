d26ba9ded1b2ea22130bf3d453afdef901a34411
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-07-16 20:02:20 +0200

    Chapter 10 ex 12

--- src/main/scala/fpinscala/monoids/monoids.scala
+++ src/main/scala/fpinscala/monoids/monoids.scala
@@ -2,6 +2,7 @@ package fpinscala.monoids
 
 import fpinscala.parallelism.Par
 import fpinscala.parallelism.Par._
+import scala.language.higherKinds
 
 trait Monoid[A] {
   def op(a1: A, a2: A): A
@@ -144,6 +145,47 @@ object Monoid {
     }
 
     override def zero: WC = Stub("")
+  }
+
+  trait Foldable[F[_]] {
+
+    def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
+    def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+
+    def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+      foldLeft(as)(mb.zero)((b, a) => mb.op(b, f(a)))
+
+    def concatenate[A](as: F[A])(m: Monoid[A]): A =
+      foldLeft(as)(m.zero)(m.op)
+  }
+
+  object ListFoldable extends Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
+      as.foldRight(z)(f)
+
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
+      as.foldLeft(z)(f)
+
+  }
+
+  object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+    override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B =
+      as.foldRight(z)(f)
+
+    override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B =
+      as.foldLeft(z)(f)
+
+    override def foldMap[A, B](as: IndexedSeq[A])(f: (A) => B)(mb: Monoid[B]): B =
+      Monoid.foldMap(as, mb)(f)
+  }
+
+  object StreamFoldable extends Foldable[Stream] {
+    override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B =
+      as.foldRight(z)(f)
+
+    override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B =
+      as.foldLeft(z)(f)
+
   }
 
 }
