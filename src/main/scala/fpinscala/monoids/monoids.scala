893a6f329e09e4295418f7e455693a5625e6f1fa
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-07-17 21:29:42 +0200

    Chapter 10 ex 18

--- src/main/scala/fpinscala/monoids/monoids.scala
+++ src/main/scala/fpinscala/monoids/monoids.scala
@@ -228,4 +228,17 @@ object Monoid {
       as.foldLeft(z)(f)
   }
 
+  def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] =
+    new Monoid[Map[K, V]] {
+      def zero = Map()
+      def op(a: Map[K, V], b: Map[K, V]) =
+        a.map {
+          case (k, v) => (k, V.op(v, b.get(k) getOrElse V.zero))
+        }
+    }
+
+  def bag[A](as: IndexedSeq[A]): Map[A, Int] =
+    //reference solution
+    foldMap(as, mapMergeMonoid[A, Int](intAddition))(a => Map(a -> 1))
+
 }
