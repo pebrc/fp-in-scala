d063f9b51ed3c7e4800626db504fcccad401ea06
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-08-03 21:15:36 +0100

    Chapter 11 ex 13

--- src/main/scala/fpinscala/monads/monads.scala
+++ src/main/scala/fpinscala/monads/monads.scala
@@ -53,6 +53,10 @@ trait Monad[F[_]] extends Functor[F] {
     flatMap(mma)(ma => ma)
   }
 
+  def flatMapViaJoin[A, B](ma: F[A])(f: A => F[B]): F[B] = {
+    join(map(ma)(f))
+  }
+
 }
 
 object Monad {
