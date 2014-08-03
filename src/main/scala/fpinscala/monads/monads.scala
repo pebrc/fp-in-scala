f49f751c5fb8bace42fbc5a18c20992badbada1c
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-08-03 21:23:53 +0100

    Chapter 11 ex 17

--- src/main/scala/fpinscala/monads/monads.scala
+++ src/main/scala/fpinscala/monads/monads.scala
@@ -94,4 +94,21 @@ object Monad {
       ma.flatMap(f)
   }
 
+  case class Id[A](value: A) {
+    def flatMap[B](f: (A) => Id[B]): Id[B] = {
+      f(value)
+    }
+
+    def map[B](f: (A) => B): Id[B] = {
+      Id(f(value))
+    }
+  }
+
+  def idMonad = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = Id(a)
+
+    override def flatMap[A, B](ma: Id[A])(f: (A) => Id[B]): Id[B] =
+      ma.flatMap(f)
+  }
+
 }
