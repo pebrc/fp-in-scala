89e7d9a096141dec130321ea7734aca937d1e107
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-08-25 22:15:00 +0200

    Chapter 12 ex 3

--- src/main/scala/fpinscala/applicative/applicative.scala
+++ src/main/scala/fpinscala/applicative/applicative.scala
@@ -35,4 +35,18 @@ trait Applicative2[F[_]] extends Functor[F] {
 
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     apply(apply(unit(f.curried))(fa))(fb)
+
+  def map3[A, B, C, D](fa: F[A],
+    fb: F[B],
+    fc: F[C])(f: (A, B, C) => D): F[D] = {
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+  }
+
+  def map4[A, B, C, D, E](fa: F[A],
+    fb: F[B],
+    fc: F[C],
+    fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
+  }
+
 }
