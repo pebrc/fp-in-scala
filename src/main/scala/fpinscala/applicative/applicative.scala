9da4d8ae605686b5b29e263d76f7cfd4fd18021b
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-08-29 18:54:35 +0200

    Chapter 12 ex 6

--- src/main/scala/fpinscala/applicative/applicative.scala
+++ src/main/scala/fpinscala/applicative/applicative.scala
@@ -1,3 +1,5 @@
+import java.util.Date
+
 import scala.language.higherKinds
 
 trait Applicative[F[_]] extends Functor[F] {
@@ -21,6 +23,23 @@ trait Applicative[F[_]] extends Functor[F] {
   }
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((_, _))
+
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)((f, a) => f(a))
+
+  def map3[A, B, C, D](fa: F[A],
+    fb: F[B],
+    fc: F[C])(f: (A, B, C) => D): F[D] = {
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+  }
+
+  def map4[A, B, C, D, E](fa: F[A],
+    fb: F[B],
+    fc: F[C],
+    fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
+  }
+
 }
 
 trait Applicative2[F[_]] extends Functor[F] {
@@ -48,5 +67,54 @@ trait Applicative2[F[_]] extends Functor[F] {
     fd: F[D])(f: (A, B, C, D) => E): F[E] = {
     apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
   }
+
+}
+
+sealed trait Validation[+E, +A]
+case class Failure[E](head: E, tail: Vector[E] = Vector()) extends Validation[E, Nothing]
+case class Success[A](a: A) extends Validation[Nothing, A]
+
+object Applicative {
+
+  def validationApplicative[E] = new Applicative[({ type f[x] = Validation[E, x] })#f] {
+
+    override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] =
+      (fa, fb) match {
+        case (Failure(h, t), Failure(h2, t2)) => Failure(h, (h2 +: t) ++ t2)
+        case (Success(_), f @ Failure(_, _)) => f
+        case (f @ Failure(_, _), Success(_)) => f
+        case (Success(a), Success(b)) => Success(f(a, b))
+      }
+
+    override def unit[A](a: => A): Validation[E, A] = Success(a)
+
+  }
+
+  val stringWebForm = validationApplicative[String]
+
+  case class WebForm(name: String, birthdate: Date, phoneNumber: String)
+
+  def validName(name: String): Validation[String, String] =
+    if (name != "") Success(name)
+    else Failure("Name cannot be empty")
+
+  def validBirthdate(birthdate: String): Validation[String, Date] =
+    try {
+      import java.text._
+      Success((new SimpleDateFormat("yyyy-MM-dd")).parse(birthdate))
+    } catch {
+      case _: Exception => Failure("Birthdate must be in the form yyyy-MM-dd")
+    }
+
+  def validPhone(phoneNumber: String): Validation[String, String] =
+    if (phoneNumber.matches("[0-9]{10}")) Success(phoneNumber)
+    else Failure("Phone number must be 10 digits")
+
+  def validWebForm(name: String, birthdate: String, phone: String): Validation[String, WebForm] =
+    stringWebForm.map3(
+      validName(name),
+      validBirthdate(birthdate),
+      validPhone(phone))(
+        WebForm(_, _, _))
 
 }
